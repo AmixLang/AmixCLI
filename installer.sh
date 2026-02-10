957f6336bbfd6ba88d907a5ffa45ff5e482a3afd
trindadedev13 <trindadedev13@gmail.com>
2026-02-10 18:31:02 -0300

    feat: Add info about installing status
    Signed-off-by: trindadedev13 <trindadedev13@gmail.com>

--- installer.sh
+++ installer.sh
@@ -7,5 +7,8 @@ VERSION="1.0.1"
 mkdir -p "$DIR"
 echo "Downloading resources..."
 wget -q "https://github.com/AmixLang/AmixCLI/releases/download/$VERSION/amixc.jar" -O "$FILE" 2>/dev/null
+echo "Downloaded!"
 
 cp ./amix $DIR/
+chmod +x $DIR/amix
+echo "Installed!"
